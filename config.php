9a5af3d929ed6bb7ec1c45900b3ccb13a438e157
Retno Dwi Pangestika <149929216+redkaapangestika@users.noreply.github.com>
2025-07-14 07:38:45 +0700

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Configuration settings for Google OAuth project
-
-define('GOOGLE_CLIENT_ID', 'your-client-id.apps.googleusercontent.com');
-define('GOOGLE_CLIENT_SECRET', 'your-client-secret');
-define('GOOGLE_REDIRECT_URI', 'http://localhost/google-oauth-project/callback.php');
-
-// Other configuration settings can be added here as needed
-?>
